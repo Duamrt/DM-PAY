1de5141193d5237d4acd0e50205d58f9662e6b8f
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-24 10:24:22 -0300

    v04241024 feat mobile: seção Crescimento YoY premium com hero card + barras + projeção 2027

--- dmpay-version.js
+++ dmpay-version.js
@@ -1,5 +1,5 @@
 // DM Pay · versao atual
-window.DMPAY_VERSION = 'v04241019';
+window.DMPAY_VERSION = 'v04241024';
 (function() {
   var el = document.createElement('div');
   el.id = 'dmpay-version-badge';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dmpay-mobile-v04241019';
+const CACHE = 'dmpay-mobile-v04241024';
 const SHELL = [
   '/DM-PAY/mobile.html',
   '/DM-PAY/js/supabase.js',
