2115168ebbdc9ff75152765ce882920b0969e588
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-27 17:09:51 -0300

    v04271709 Cobranca: pagina gestao de cobranças + sidebar Alertas→Cobrança em todos os HTMLs

--- dmpay-version.js
+++ dmpay-version.js
@@ -1,5 +1,5 @@
 // DM Pay · versao atual
-window.DMPAY_VERSION = 'v04271320';
+window.DMPAY_VERSION = 'v04271709';
 (function() {
   var el = document.createElement('div');
   el.id = 'dmpay-version-badge';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dmpay-mobile-v04271320';
+const CACHE = 'dmpay-mobile-v04271709';
 const SHELL = [
   '/DM-PAY/mobile.html',
   '/DM-PAY/js/supabase.js',
