80ca5b145cb2d7c7fcd6515e50b782436d5e65a9
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-28 17:00:36 -0300

    v04281700 cobrança: default Este ano + % inadimplência real sem cap

--- dmpay-version.js
+++ dmpay-version.js
@@ -1,5 +1,5 @@
 // DM Pay · versao atual
-window.DMPAY_VERSION = 'v04281651';
+window.DMPAY_VERSION = 'v04281700';
 (function() {
   var el = document.createElement('div');
   el.id = 'dmpay-version-badge';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dmpay-mobile-v04281651';
+const CACHE = 'dmpay-mobile-v04281700';
 const SHELL = [
   '/DM-PAY/mobile.html',
   '/DM-PAY/js/supabase.js',
