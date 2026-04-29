26e1b7bfa29fca5dda77d6051e3623c6420f392c
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-29 17:38:52 -0300

    v04291738 fix: css dropdown status contas a pagar igual receber

--- dmpay-version.js
+++ dmpay-version.js
@@ -1,5 +1,5 @@
 // DM Pay · versao atual
-window.DMPAY_VERSION = 'v04291735';
+window.DMPAY_VERSION = 'v04291738';
 (function() {
   var el = document.createElement('div');
   el.id = 'dmpay-version-badge';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dmpay-mobile-v04291735';
+const CACHE = 'dmpay-mobile-v04291738';
 const SHELL = [
   '/DM-PAY/mobile.html',
   '/DM-PAY/js/supabase.js',
