2a6226d7e3d8a448ed6f32d29a4c6d88edcccc6b
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-29 08:45:48 -0300

    v04290845 fix: adiciona Evolucao Anual no sidebar do DRE

--- dmpay-version.js
+++ dmpay-version.js
@@ -1,5 +1,5 @@
 // DM Pay · versao atual
-window.DMPAY_VERSION = 'v04290834';
+window.DMPAY_VERSION = 'v04290845';
 (function() {
   var el = document.createElement('div');
   el.id = 'dmpay-version-badge';
