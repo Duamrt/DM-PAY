a24766b7184211970402ba6db1a26ac784f6089d
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-29 05:01:38 -0300

    v04290501 fix mobile nav data + register_sessions + sangria fix

--- dmpay-version.js
+++ dmpay-version.js
@@ -1,5 +1,5 @@
 // DM Pay · versao atual
-window.DMPAY_VERSION = 'v04281706';
+window.DMPAY_VERSION = 'v04290501';
 (function() {
   var el = document.createElement('div');
   el.id = 'dmpay-version-badge';
