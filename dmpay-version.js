ffbe7533042e0ec42fe1e77d6f4d5501599360d2
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-30 05:05:39 -0300

    v04300505 fix: dropdown filtro status Contas a Pagar respeita tema (dark/light)

--- dmpay-version.js
+++ dmpay-version.js
@@ -1,5 +1,5 @@
 // DM Pay · versao atual
-window.DMPAY_VERSION = 'v04300458';
+window.DMPAY_VERSION = 'v04300505';
 (function() {
   var el = document.createElement('div');
   el.id = 'dmpay-version-badge';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dmpay-mobile-v04300458';
+const CACHE = 'dmpay-mobile-v04300505';
 const SHELL = [
   '/DM-PAY/mobile.html',
   '/DM-PAY/js/supabase.js',
