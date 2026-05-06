c05e8c3fc8452f1bc1089e5a846ea8c2a4c85a44
Duam Rodrigues <3drxdzin@gmail.com>
2026-05-06 08:22:25 -0300

    v05060822 feat(privacy): mascarar texto nas gravacoes PostHog (LGPD)

--- dmpay-version.js
+++ dmpay-version.js
@@ -1,5 +1,5 @@
 // DM Pay · versao atual + auto-update
-window.DMPAY_VERSION = 'v05042152';
+window.DMPAY_VERSION = 'v05060822';
 (function() {
   // ------- Badge de versao no canto -------
   var el = document.createElement('div');
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dmpay-mobile-v05042152';
+const CACHE = 'dmpay-mobile-v05060822';
 
 self.addEventListener('install', e => {
   // Ativa novo SW imediatamente, sem esperar fechar abas
